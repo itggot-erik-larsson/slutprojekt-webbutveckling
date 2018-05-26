7c75aec96f41870b894e015b200397e4ddf5d146
itggot-erik-larsson <35598503+itggot-erik-larsson@users.noreply.github.com>
2018-05-26 12:00:32 +0200

    yeet

---
+++ site/js/menu.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    var element = document.querySelector(".menu");
+    element.classList.toggle("show");
+
+    element = document.querySelector(".menu_button");
+    element.classList.toggle("fade")
+}
